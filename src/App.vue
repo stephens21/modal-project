e1bc8c6371ee21111acebfeb4ad9d0ef5759331e
stephens21 <stephenstalla21@gmail.com>
2024-11-27 09:47:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import Modal from './components/Modal.vue'
+export default{
+  name: 'App',
+  components: {Modal},
+  data(){
+    return {
+      title : 'My First Vue App :)',
+      header:'Sign up for the giveaway',
+      text:'Grab your ninja swag for half price !'
+    }
+  },
+  methods: {
+ 
+  }
+}
+</script>
+
+<template>
+
+
+  <main>
+    <h1>{{title }}</h1>
+   <Modal :header="header" :text="text"/>
+  </main>
+</template>
+
+
+<style>
+body{
+  background: white;
+}
+#app{
+  font-family: Avenir, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px;
+}
+
+h1{
+  display: inline-block;
+}
+
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['header', 'text']
+}
+</script>
+
+<style scoped>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+h1{
+    color: aqua;
+    border: none;
+}
+</style>
